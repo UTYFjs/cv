29b8336df7ad52a7ac7548cc7375d974bd1972f7
UTYFjs <utyf-90@mail.ru>
2024-06-08 10:11:51 +0300

    feat: add tabs component

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -3,30 +3,49 @@ import styles from './Projects.module.scss';
 import { PROJECT_REACT_DATA } from '~/constants/projects-constants';
 import cn from 'classnames';
 import { useEffect, useState } from 'react';
+import { Tabs } from '~/components/tabs/tabs';
 
 export const ProjectsPage = () => {
   const [animation, setAnimation] = useState(false);
   useEffect(() => {
     setAnimation(true);
+    return () => {
+      setAnimation(false);
+    };
   }, []);
   const cardClassnames = cn(styles.card, animation && styles.show);
 
   return (
-    <div className={styles['projects_react']}>
-      {PROJECT_REACT_DATA.map(({ id, title, src, shortDescription, description, linkDeploy, linkCode, stack }) => (
-        <ProjectCard
-          key={id}
-          className={cardClassnames}
-          title={title}
-          src={src}
-          shortDescription={shortDescription}
-          description={description}
-          linkDeploy={linkDeploy}
-          linkCode={linkCode}
-          stack={stack}
-          size={'medium'}
-        />
-      ))}
-    </div>
+    <>
+      <Tabs
+        tabs={[
+          {
+            label: 'React',
+            content: (
+              <div className={styles['projects_react']}>
+                {PROJECT_REACT_DATA.map(
+                  ({ id, title, src, shortDescription, description, linkDeploy, linkCode, stack }) => (
+                    <ProjectCard
+                      key={id}
+                      className={cardClassnames}
+                      title={title}
+                      src={src}
+                      shortDescription={shortDescription}
+                      description={description}
+                      linkDeploy={linkDeploy}
+                      linkCode={linkCode}
+                      stack={stack}
+                      size={'medium'}
+                    />
+                  )
+                )}
+              </div>
+            ),
+          },
+          { label: 'JS, HTML, CSS', content: <p>content tab 2</p> },
+          { label: 'Node.JS', content: <p>content tab 3</p> },
+        ]}
+      />
+    </>
   );
 };
