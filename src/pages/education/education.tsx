a9cdf55c1f2bcd5e4672ae4540fe3fbfe8f38c50
UTYFjs <utyf-90@mail.ru>
2024-06-25 16:50:19 +0300

    feat: education card component, add  high-education

--- src/pages/education/education.tsx
+++ src/pages/education/education.tsx
@@ -1,6 +1,7 @@
 import { EducationCard } from '~/components/education-card/education-card';
 import styles from './education.module.scss';
-import { nanoid } from 'nanoid';
+import { useEffect, useRef } from 'react';
+import { certificate } from '~/constants/education-contants';
 
 export type CertificateType = {
   id: string;
@@ -9,57 +10,52 @@ export type CertificateType = {
   title: string;
   schoolLink: string;
 };
-const certificate: CertificateType[] = [
-  {
-    id: nanoid(),
-    imgSrc: './image/education/algo-5.0.webp',
-    link: 'https://certify.s3.yandex.net/young-yandex/c1f747d9-e377-4f81-bb67-3e72d31660c1/8d44af91-4899-4efa-9a53-204f0c2157d1.pdf?m-message-key-id=-207165574941442048&m-message-click-id=0ed18715-5825-40ba-9b24-84b765389c2a&utm_source=mindbox&utm_medium=email&utm_campaign=training5&utm_content=certificate',
-    title: 'Тренировки по алгоритмам 5.0 / Yandex',
-    schoolLink: '',
-  },
-  {
-    id: nanoid(),
-    imgSrc: './image/education/marathon.webp',
-    link: 'https://drive.google.com/file/d/1-GZgimSdZRsDWnkMi5WdG24Eqllr5DBA/view',
-    title: 'Frontend Marathon / Clevertech',
-    schoolLink: '',
-  },
-  {
-    id: nanoid(),
-    imgSrc: './image/education/nodejs.webp',
-    link: 'https://app.rs.school/certificate/ax1nhr0f',
-    title: 'NodeJS / RSSchool',
-    schoolLink: '',
-  },
-  {
-    id: nanoid(),
-    imgSrc: './image/education/react.webp',
-    link: 'https://app.rs.school/certificate/pi9lql0v',
-    title: 'React / RSSchool',
-    schoolLink: '',
-  },
-  {
-    id: nanoid(),
-    imgSrc: './image/education/frontend-javascript.webp',
-    link: 'https://app.rs.school/certificate/os3uap3f',
-    title: 'Frontend/Javascript / RSSchool',
-    schoolLink: '',
-  },
-];
 
-export const Education = () => (
-  <div className={styles.education}>
-    <p className={styles.title}> Образование и курсы</p>
-    <div className={styles['education-wrapper']}>
-      {certificate.map((item) => (
-        <EducationCard
-          key={item.id}
-          cardData={item}
-          onClick={() => {
-            console.log('helloworld');
-          }}
-        />
-      ))}
+export const Education = () => {
+  const elementRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const callback: IntersectionObserverCallback = (entries) => {
+      entries.forEach((item) => {
+        console.log(item);
+        if (item.intersectionRatio > 0.1) {
+          console.log(item);
+          item.target.classList.add(styles['in-viewport']);
+        }
+      });
+    };
+    const element = elementRef.current;
+    if (element) {
+      const observer = new IntersectionObserver(callback);
+      observer.observe(element);
+    }
+  }, []);
+
+  return (
+    <div className={styles.education}>
+      <p className={styles.title}> Образование и курсы</p>
+      <div className={styles['education-wrapper']}>
+        {certificate.map((item) => (
+          <EducationCard
+            key={item.id}
+            cardData={item}
+            onClick={() => {
+              console.log('helloworld');
+            }}
+          />
+        ))}
+      </div>
+      <div className={styles['high-education-wrapper']} ref={elementRef}>
+        <div>
+          <span className={styles['year-start']}>2007</span> <span className={styles['year-end']}>2012</span>
+        </div>
+        <div>
+          <p className={styles.university}> Белорусский Государственный Университет</p>
+          <p className={styles.speciality}> Инженер-геолог</p>
+          <p className={styles.speciality}> Геология и разведка месторождений полезных ископаемых</p>
+        </div>
+        <div className={styles.blur}></div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
