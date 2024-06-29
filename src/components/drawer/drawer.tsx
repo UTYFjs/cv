7732a1452d1714b0f8ae5b8a5560d18470752528
UTYFjs <utyf-90@mail.ru>
2024-06-29 13:39:45 +0300

    feat: add drawer component

---
+++ src/components/drawer/drawer.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useRef } from 'react';
+import type { CSSProperties } from 'react';
+import cn from 'classnames';
+import styles from './drawer.module.scss';
+import { CloseXSvg } from '../custom-icons/CustomIcons';
+
+type IDirection = 'left' | 'right' | 'top' | 'bottom';
+
+type DrawerProps = {
+  isOpen: boolean;
+  direction: IDirection;
+  onClose?: VoidFunction;
+  children?: React.ReactNode;
+  duration?: number;
+  overlayStyles?: CSSProperties;
+  enableOverlay?: boolean;
+  style?: CSSProperties;
+  zIndex?: number;
+  size?: number | string;
+  className?: string;
+  overlayClassName?: string;
+};
+
+type DirectionStyle = Pick<CSSProperties, 'top' | 'left' | 'right' | 'bottom' | 'width' | 'height' | 'transform'>;
+
+const getDirectionStyle = (dir: IDirection, size?: number | string): DirectionStyle => {
+  const directionStyle: Record<IDirection, DirectionStyle> = {
+    left: {
+      top: 0,
+      left: 0,
+      transform: 'translate(-100%, 0)',
+      width: size,
+      height: '100vh',
+    },
+    right: {
+      top: 0,
+      right: 0,
+      transform: 'translate(100%, 0)',
+      width: size,
+      height: '100vh',
+    },
+    bottom: {
+      left: 0,
+      right: 0,
+      bottom: 0,
+      transform: 'translate(0, 100%)',
+      width: '100%',
+      height: size,
+    },
+    top: {
+      left: 0,
+      right: 0,
+      top: 0,
+      transform: 'translate(0, -100%)',
+      width: '100%',
+      height: size,
+    },
+  };
+  return directionStyle[dir];
+};
+
+export const Drawer = ({
+  isOpen,
+  direction,
+  onClose = () => {},
+  children,
+  style,
+  enableOverlay = true,
+  overlayStyles,
+  zIndex = 2,
+  duration = 500,
+  size = 'fit-content',
+  className,
+  overlayClassName = '',
+}: DrawerProps) => {
+  const bodyRef = useRef<HTMLBodyElement | null>(null);
+  useEffect(() => {
+    const updatePageScroll = () => {
+      bodyRef.current = window.document.querySelector('body');
+      if (bodyRef.current) {
+        bodyRef.current.style.overflow = isOpen ? 'hidden' : '';
+      }
+    };
+    updatePageScroll();
+  }, [isOpen]);
+
+  const drawerStyles: CSSProperties = {
+    zIndex: zIndex + 1,
+    transition: `all ${duration}ms`,
+    ...getDirectionStyle(direction, size),
+    ...style,
+  };
+
+  return (
+    <div className={cn(styles.drawer, isOpen && styles.active)}>
+      <div
+        style={{ ...drawerStyles, transform: isOpen ? 'translate(0, 0)' : drawerStyles.transform }}
+        className={cn(styles['drawer_container'], styles[direction], className && styles[className])}
+      >
+        <CloseXSvg className={styles.close} height={'32px'} width={'32px'} onClick={onClose} />
+        {children}
+      </div>
+      {enableOverlay && (
+        <div
+          className={cn(styles['drawer_overlay'], overlayClassName && styles[overlayClassName])}
+          onClick={onClose}
+          style={overlayStyles}
+        />
+      )}
+    </div>
+  );
+};
--- src/pages/layout-main/layout-main.tsx
+++ src/pages/layout-main/layout-main.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from '~/components/header/Header';
+import { Button } from '~/components/button/Button';
+import { Drawer } from '~/components/drawer/drawer';
 
-export const LayoutMain = () => (
-  <>
-    {' '}
-    <Header />
-    <Outlet />
-  </>
-);
+import { Header } from '~/components/header/Header';
+import { Menu } from '~/components/menu/Menu';
+
+export const LayoutMain = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  useEffect(() => {
+    console.log('isdrawerOpen', isDrawerOpen);
+  }, [isDrawerOpen]);
+  return (
+    <>
+      {' '}
+      <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} direction={'top'}>
+        {' '}
+        <Menu />
+      </Drawer>
+      <Button onClick={() => setIsDrawerOpen((state) => !state)}>Open Drawer2</Button>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
